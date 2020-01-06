ffece1f929632ec6df11d02f56fa501916268b6a
Yinghumi <1984363619@qq.com>
2020-01-06 21:10:18 +0800

    yes

--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -1,18 +1,5 @@
 <template>
   <div>
-     <swiper
-      :indicator-dots="indicatorDots"
-      :autoplay="autoplay"
-      :interval="interval"
-      :duration="duration"
-      style="height:200px"
-    >
-    <block v-for="item in imgUrls" :key="item">
-      <swiper-item>
-        <image :src="item" style="width:100%;"/>
-      </swiper-item>
-    </block>
-  </swiper>
     <i-grid i-class="no-border">
     <i-grid-item @click="goType(grid)" v-for="grid in grids" :key="grid" i-class="no-border">
         <i-grid-icon>
@@ -49,11 +36,6 @@ export default {
              {"name":"《南方车站的聚会》","role":"humi","image":"http://wx2.sinaimg.cn/large/006MYXpkgy1g9g158yilsj30u00gv7wh.jpg","remark":"影片故事灵感来源于真实新闻事件，讲述了偷车团伙头目周泽农，在重金悬赏下走上逃亡之路，艰难寻求自我救赎的故事 。"},
              {"name":"《攀登者》","role":"歌以咏志","image":"http://n.sinaimg.cn/sinacn20191122ac/733/w469h264/20191122/004f-iittafq8672618.jpg","remark":"影片以1960年与1975年中国登山队两次登顶珠峰的事迹为背景，讲述方五洲、曲松林等中国攀登者怀揣着最纯粹的梦想集结于珠峰，肩负时代使命于世界之巅的故事。"}
              ],
-           imgUrls: [
-    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1578137639960&di=1c0c229986364b80ebbbcdba665db048&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F933b3ee259a9196b6b796d9ee9c88a177c06614c3bd35-xkZ0vw_fw658',
-    'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2138540041,3871512787&fm=26&gp=0.jpg',
-    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1578137748150&di=fe6787124f511c662c04dfe005f8b03f&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201603%2F17%2F20160317164103_FEySx.jpeg'
-      ],
       indicatorDots: true,
       autoplay: true,
       interval: 5000,
