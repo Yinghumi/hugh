86dd1e8d1d25ed4918388711c8475bf72671c202
Yinghumi <1984363619@qq.com>
2020-01-06 19:32:31 +0800

    页面12

--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -31,11 +31,11 @@ export default {
           {title:"话剧",image:"/static/images/4.png"},
         ],
         top:[
-          {name:"名称1",role:"回复1",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"评论1"},
-          {name:"名称2",role:"回复2",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"评论2"},
-          {name:"名称3",role:"回复3",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"评论3"},
-          {name:"名称4",role:"回复4",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"评论4"}
-        ],
+          {"name":"《辛亥革命》","role":"回复：","image":"http://5b0988e595225.cdn.sohucs.com/images/20180126/db1e97ebec88481384e7c87457481e46.jpeg","remark":"这个革命战争太催泪了，实在是忍不住，大家都很棒啊。"},
+          {"name":"《你好，之华》","role":"回复：","image":"http://wx4.sinaimg.cn/wap720/41de2dbdly1fx5j47s0ncj21be0qo7c5.jpg","remark":"我胡在里面演坏人，还记得他说他想尝试新的角色，他做到了！"},
+          {"name":"《南方车站的聚会》","role":"回复：","image":"http://wx2.sinaimg.cn/large/006MYXpkgy1g9g158yilsj30u00gv7wh.jpg","remark":"自我救赎，某些镜头过于可怕，但是各位演员们演技都很好 。"},
+          {"name":"《攀登者》","role":"回复：","image":"http://n.sinaimg.cn/sinacn20191122ac/733/w469h264/20191122/004f-iittafq8672618.jpg","remark":"搞不懂为什么要把自己的东西给别人盖弄得自己截肢，不管是电影还是现实都很心疼。。。"}
+          ],
           
     }
   },
